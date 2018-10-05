b876447fc05e5367099a8fe859cce33d5a19bf2a
Deivy Hernandez <dhernandez@sipcom.cl>
2018-10-05 16:22:23 -0300

    Buscar run, con listado de visitas

--- resources/views/condominium/fragment/searchRun.blade.php
+++ resources/views/condominium/fragment/searchRun.blade.php
@@ -8,19 +8,40 @@
                           </button>
                         </div>
                         <div class="modal-body">
-                        <div id="searchVisitor" class="col-lg-6 mx-auto">
-                                <div class="input-group">
-                                        <input type="text" name="rutVisitor" id="rutVisitor" style="text-transform:uppercase" maxlength="9" class="form-control" placeholder="Run" >
-                                        <div class="input-group-append">
-                                          
-                                          <button class="input-group-text" id="searchRun" type="button">Buscar</button>
+                                <div class="row">
+                                        <div id="searchVisitor" class="col-lg-6 mx-auto">
+                                                <div class="input-group">
+                                                        <input type="text" name="rutVisitor" id="rutVisitor" style="text-transform:uppercase" maxlength="9" class="form-control" placeholder="Run" >
+                                                        <div class="input-group-append">
+                                                        <button class="input-group-text" id="searchRun" type="button">Buscar</button>
+                                                        </div>
+                                                </div>
+                                        </div> 
+                                </div>
+                        
+                                <div class="row">
+                                        <div class="col text-center">
+                                                Visitas Recientes
                                         </div>
                                 </div>
-
                                 <div class="row">
-                                        <div class="">Visitas Recientes</div>
+                                        <div class="col text-center">
+                                        <table>
+                                                <thead>
+                                                        <tr>
+                                                                <td>Name</td>
+                                                                <td>Office</td>
+                                                        </tr>
+                                                </thead>
+                                                <tbody>
+                                                        <tr>
+                                                                <td>Julio</td>
+                                                                <td>Sipcom</td>
+                                                        </tr>
+                                                </tbody>
+                                        </table>
+                                        </div>
                                 </div>
-                        </div> 
                         </div>
                 </div>
         </div>
